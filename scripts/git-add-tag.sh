8215fc5ff17fd40ec39c82823ba1536c6516bce1
shion <tenta_shion@yahoo.co.jp>
2022-01-19 22:58:06 +0900

    GitHub Actions の追加

--- scripts/git-add-tag.sh
+++ scripts/git-add-tag.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# 最新コミット日時の出力
-echo "Latest commit date: $(git log -1 --date=iso-strict --format='%cd')"
-
 # 最新タグの確認
 latestTag=$(git describe --tags --abbrev=0)
 latestTag=${latestTag:-'none'}
 echo "Latest tag: $latestTag"
 
 # コミットタグ候補の生成
-tmpTag=$(git log -1 --date=format-local:'%Y.%m.%d' --format='%cd')
+tmpTag=$(git log main -1 --date=format-local:'%Y.%m.%d' --format='%cd')
 echo "Tag candidate: $tmpTag"
 
 # タグの付与
