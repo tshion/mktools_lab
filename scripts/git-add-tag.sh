1b7f3b27e00438262b8a9cbc6abc7ca0dcfa41d0
shion <tenta_shion@yahoo.co.jp>
2022-01-22 09:11:27 +0900

    [git]現在のブランチの出力追加

--- scripts/git-add-tag.sh
+++ scripts/git-add-tag.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# 現在のブランチの出力
+currentBranch=$(git branch --contains | cut -d " " -f 2)
+echo "Current branch: $currentBranch"
+
 # 最新コミット日時の出力
-echo "Latest commit date: $(git log main -1 --date=iso-strict --format='%cd')"
+echo "Latest commit date: $(git log $currentBranch -1 --date=iso-strict --format='%cd')"
 
 # 最新タグの確認
-latestTag=$(git describe main --tags --abbrev=0)
+latestTag=$(git describe $currentBranch --tags --abbrev=0)
 latestTag=${latestTag:-'none'}
 echo "Latest tag: $latestTag"
 
 # コミットタグ候補の生成
-tmpTag=$(git log main -1 --date=format-local:'%Y.%m.%d' --format='%cd')
+tmpTag=$(git log $currentBranch -1 --date=format-local:'%Y.%m.%d' --format='%cd')
 echo "Tag candidate: $tmpTag"
 
 # タグの付与
