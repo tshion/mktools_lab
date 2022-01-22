3801983f014920c0253431df80c82c7443b1ea19
shion <tenta_shion@yahoo.co.jp>
2022-01-22 09:05:53 +0900

    [git]ブランチ指定の追加

--- scripts/git-add-tag.sh
+++ scripts/git-add-tag.sh
@@ -4,7 +4,7 @@
 echo "Latest commit date: $(git log main -1 --date=iso-strict --format='%cd')"
 
 # 最新タグの確認
-latestTag=$(git describe --tags --abbrev=0)
+latestTag=$(git describe main --tags --abbrev=0)
 latestTag=${latestTag:-'none'}
 echo "Latest tag: $latestTag"
 
