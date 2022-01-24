8cdb329aace9a8acf67566444ec8c352f002492a
shion <tenta_shion@yahoo.co.jp>
2022-01-25 00:38:21 +0900

    テンプレートデータの仮実装

--- generators/HttpStatusCodeGenerator/Program.cs
+++ generators/HttpStatusCodeGenerator/Program.cs
@@ -1,12 +1,30 @@
 // See https://aka.ms/new-console-template for more information
 using HttpStatusCodeGenerator;
 using System.Collections.Immutable;
+using System.Text;
 
 try
 {
     Console.WriteLine("Start");
     var basePath = Directory.GetCurrentDirectory();
     var dataBasePath = Path.Combine(basePath, "Data");
+
+
+    // テンプレートファイルの読み込み
+    List<TemplateEntity> templates = new[] { "cs" }
+        .Select(ext => TemplateEntity.LoadOrNull(
+            basePath: Path.Combine(basePath, "Templates"),
+            fileExtension: ext,
+            indent: 4
+        ))
+        .Where(item => item != null)
+        .Select(item => item!)
+        .ToList();
+    if (!templates.Any())
+    {
+        Console.WriteLine("Finish: None targets.");
+        return;
+    }
 
 
     // CSV の読み込み
@@ -42,52 +60,57 @@ try
 
 
     // コード生成
-    var templatePaths = new[] { "cs" }
-        .Select(ext => TemplatePathEntity.ParseOrNull(basePath, ext))
-        .Where(item => item != null)
-        .ToList();
-    foreach (var item in templatePaths)
+    foreach (var template in templates)
     {
-        var itemFile = File.ReadAllText(item.ItemPath);
-        var itemList = new List<string>();
-        foreach (var target in data)
+        List<string> items = new();
+        var shouldAddImport = false;
+        foreach (var item in data)
         {
-            var links = target.Links
-                .Select(link => $"        ///     <item><see href=\"{link.Url}\">{link.Title}</see></item>");
-
-            var warning = !string.IsNullOrWhiteSpace(target.Warning)
-                ? $"[ObsoleteAttribute(\"{target.Warning}\")]{Environment.NewLine}        "
-                : "";
-
-            var words = new List<string>();
-            for (int i = 0; i < target.MemberWords.Length; i++)
+            // TODO: null 伝搬の見直し
+            if (item == null)
             {
-                var word = target.MemberWords[i];
-                words.Add(
-                    i == 0 && word.Length < 2
-                        ? word.ToUpper()
-                        : $"{char.ToUpper(word[0])}{word[1..]}"
+                continue;
+            }
+
+            // TODO: 各言語固有設定の分離
+            var links = item.Links
+                .Select(link => $"{template.Indent}{template.Indent}///{template.Indent}<item><see href=\"{link.Url}\">{link.Title}</see></item>");
+
+            string? warning = null;
+            if (!string.IsNullOrWhiteSpace(item.Warning))
+            {
+                shouldAddImport = true;
+                warning = $"[ObsoleteAttribute(\"{item.Warning}\")]{Environment.NewLine}{template.Indent}{template.Indent}";
+            }
+
+            StringBuilder builder = new();
+            for (int i = 0; i < item.MemberWords.Length; i++)
+            {
+                string word = item.MemberWords[i];
+                builder.Append(i == 0 && word.Length < 2
+                    ? word.ToUpper()
+                    : $"{char.ToUpper(word[0])}{word[1..]}"
                 );
             }
 
-            itemList.Add($"{itemFile}"
-                .Replace("%%DOCS_LINKS%%", string.Join(Environment.NewLine, links))
-                .Replace("%%DOCS_TITLE%%", target.Title)
-                .Replace("%%MEMBER_NAME%%", string.Join("", words))
-                .Replace("%%MEMBER_VALUE%%", target.MemberValue.ToString())
-                .Replace("%%WARNING%%", warning)
-            );
+            items.Add(template.GetItemText(
+                linkText: string.Join(Environment.NewLine, links),
+                memberName: builder.ToString(),
+                memberValue: $"{item.MemberValue}",
+                title: item.Title,
+                warning: warning ?? ""
+            ));
         }
 
-        var rootFile = File.ReadAllText(item.RootPath);
-        rootFile = rootFile.Replace("%%ITEMS%%", string.Join($"{Environment.NewLine}{Environment.NewLine}", itemList));
-        if (data.Any(x => !string.IsNullOrWhiteSpace(x.Warning)))
-        {
-            rootFile = $"using System;{Environment.NewLine}{Environment.NewLine}{rootFile}";
-        }
-        await File.WriteAllTextAsync(Path.Combine(outputBasePath, item.OutputFilename), rootFile);
+
+        string header = shouldAddImport
+            ? $"using System;{Environment.NewLine}{Environment.NewLine}"
+            : "";
+        File.WriteAllText(
+            Path.Combine(outputBasePath, template.OutputFilename),
+            $"{header}{template.GetText(string.Join($"{Environment.NewLine}{Environment.NewLine}", items))}"
+        );
     }
-
 
     Console.WriteLine("Finish: Success");
 }
