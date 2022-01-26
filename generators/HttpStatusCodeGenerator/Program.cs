2df710f2f4709b0a7300c53052f617d0b53a4c13
shion <tenta_shion@yahoo.co.jp>
2022-01-26 13:59:31 +0900

    不要なtry-catch の削除

--- generators/HttpStatusCodeGenerator/Program.cs
+++ generators/HttpStatusCodeGenerator/Program.cs
@@ -2,116 +2,111 @@
 using HttpStatusCodeGenerator;
 using System.Collections.Immutable;
 
-try
+
+Console.WriteLine("Start");
+string basePath = Directory.GetCurrentDirectory();
+
+
+// コード生成器の作成
+string templateBasePath = Path.Combine(basePath, "Templates");
+ImmutableDictionary<string, EnumCodeGenerator> generators = new Dictionary<string, EnumCodeGenerator?>()
 {
-    Console.WriteLine("Start");
-    string basePath = Directory.GetCurrentDirectory();
-
-
-    // コード生成器の作成
-    string templateBasePath = Path.Combine(basePath, "Templates");
-    ImmutableDictionary<string, EnumCodeGenerator> generators = new Dictionary<string, EnumCodeGenerator?>()
-    {
-        ["cs"] = EnumCodeGenerator.LoadOrNull(
-            actionFormatLinks: (item, indent) => string.Join(
-                Environment.NewLine,
-                item.Links.Select(x => $"{indent}{indent}/// {indent}<item><see href=\"{x.Value}\">{x.Key}</see></item>")
-            ),
-            actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
-                ? $"[Obsolete(\"{item.Warning}\")]{Environment.NewLine}{indent}{indent}"
-                : "",
-            actionGenerated: (candidate, list, indent) => list.Any(item => !string.IsNullOrWhiteSpace(item.Warning))
-                ? $"using System;{Environment.NewLine}{Environment.NewLine}{candidate}"
-                : candidate,
-            indentSize: 4,
-            memberNameType: NameType.Pascal,
-            templateItemPath: Path.Combine(templateBasePath, "HttpStatusCode.item.template.cs"),
-            templateRootPath: Path.Combine(templateBasePath, "HttpStatusCode.root.template.cs")
+    ["cs"] = EnumCodeGenerator.LoadOrNull(
+        actionFormatLinks: (item, indent) => string.Join(
+            Environment.NewLine,
+            item.Links.Select(x => $"{indent}{indent}/// {indent}<item><see href=\"{x.Value}\">{x.Key}</see></item>")
         ),
-        ["kt"] = EnumCodeGenerator.LoadOrNull(
-            actionFormatLinks: (item, indent) => string.Join(
-                Environment.NewLine,
-                item.Links.Select(x => $"{indent} * * [{x.Key}]({x.Value})")
-            ),
-            actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
-                ? $"@Deprecated(\"{item.Warning}\"){Environment.NewLine}{indent}"
-                : "",
-            actionGenerated: null,
-            indentSize: 4,
-            memberNameType: NameType.Snake,
-            templateItemPath: Path.Combine(templateBasePath, "item.template.kt"),
-            templateRootPath: Path.Combine(templateBasePath, "root.template.kt")
+        actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
+            ? $"[Obsolete(\"{item.Warning}\")]{Environment.NewLine}{indent}{indent}"
+            : "",
+        actionGenerated: (candidate, list, indent) => list.Any(item => !string.IsNullOrWhiteSpace(item.Warning))
+            ? $"using System;{Environment.NewLine}{Environment.NewLine}{candidate}"
+            : candidate,
+        indentSize: 4,
+        memberNameType: NameType.Pascal,
+        templateItemPath: Path.Combine(templateBasePath, "HttpStatusCode.item.template.cs"),
+        templateRootPath: Path.Combine(templateBasePath, "HttpStatusCode.root.template.cs")
+    ),
+    ["kt"] = EnumCodeGenerator.LoadOrNull(
+        actionFormatLinks: (item, indent) => string.Join(
+            Environment.NewLine,
+            item.Links.Select(x => $"{indent} * * [{x.Key}]({x.Value})")
         ),
-        ["swift"] = EnumCodeGenerator.LoadOrNull(
-            actionFormatLinks: (item, indent) => string.Join(
-               Environment.NewLine,
-               item.Links.Select(x => $"{indent}   {indent}- [{x.Key}]({x.Value})")
-            ),
-            actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
-                ? $"{Environment.NewLine}{Environment.NewLine}{indent} - Warning: {item.Warning}"
-                : "",
-            actionGenerated: null,
-            indentSize: 4,
-            memberNameType: NameType.Camel,
-            templateItemPath: Path.Combine(templateBasePath, "item.template.swift"),
-            templateRootPath: Path.Combine(templateBasePath, "root.template.swift")
+        actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
+            ? $"@Deprecated(\"{item.Warning}\"){Environment.NewLine}{indent}"
+            : "",
+        actionGenerated: null,
+        indentSize: 4,
+        memberNameType: NameType.Snake,
+        templateItemPath: Path.Combine(templateBasePath, "item.template.kt"),
+        templateRootPath: Path.Combine(templateBasePath, "root.template.kt")
+    ),
+    ["swift"] = EnumCodeGenerator.LoadOrNull(
+        actionFormatLinks: (item, indent) => string.Join(
+           Environment.NewLine,
+           item.Links.Select(x => $"{indent}   {indent}- [{x.Key}]({x.Value})")
         ),
-    }
-    .Where(pair => pair.Value != null)
-    .ToImmutableDictionary(pair => pair.Key, pair => pair.Value!);
-    if (!generators.Any())
-    {
-        Console.WriteLine("Finish: None targets.");
-        return;
-    }
-
-
-    // CSV の読み込み
-    string dataBasePath = Path.Combine(basePath, "Data");
-    var queryCsvBody = CsvEntity.GetDataQueryOrNull(Path.Combine(dataBasePath, "HttpStatusCode.csv"));
-    if (queryCsvBody == null)
-    {
-        Console.WriteLine("Finish: Not enough.");
-        return;
-    }
-
-
-    // 置換文字列の辞書作成
-    var queryRenameWords = WordEntity.GetDataQueryOrNull(Path.Combine(dataBasePath, "ReplaceDictionary.csv"));
-    ImmutableDictionary<string, string> renameWords = queryRenameWords != null
-        ? queryRenameWords.ToImmutableDictionary(item => item.Target, item => item.Rename)
-        : ImmutableDictionary<string, string>.Empty;
-
-
-    // データの解析
-    ImmutableList<ContentEntity> data = queryCsvBody
-        .Select(item => ContentEntity.Parse(item, renameWords))
-        .Where(item => item != null)
-        .Select(item => item!)
-        .ToImmutableList();
-
-
-    // 出力先のディレクトリ再作成
-    string outputBasePath = Path.Combine(basePath, "DataOutput");
-    if (Directory.Exists(outputBasePath))
-    {
-        Directory.Delete(outputBasePath, true);
-    }
-    Directory.CreateDirectory(outputBasePath);
-
-
-    // コード生成
-    foreach (var (ext, generator) in generators)
-    {
-        File.WriteAllText(
-            Path.Combine(outputBasePath, $"HttpStatusCode.{ext}"),
-            generator.Generate(data)
-        );
-    }
-
-    Console.WriteLine("Finish: Success");
+        actionFormatWarning: (item, indent) => !string.IsNullOrWhiteSpace(item.Warning)
+            ? $"{Environment.NewLine}{Environment.NewLine}{indent} - Warning: {item.Warning}"
+            : "",
+        actionGenerated: null,
+        indentSize: 4,
+        memberNameType: NameType.Camel,
+        templateItemPath: Path.Combine(templateBasePath, "item.template.swift"),
+        templateRootPath: Path.Combine(templateBasePath, "root.template.swift")
+    ),
 }
-catch (Exception ex)
+.Where(pair => pair.Value != null)
+.ToImmutableDictionary(pair => pair.Key, pair => pair.Value!);
+if (!generators.Any())
 {
-    Console.WriteLine($"Error: {ex.Message}");
+    Console.WriteLine("Finish: None targets.");
+    return;
 }
+
+
+// CSV の読み込み
+string dataBasePath = Path.Combine(basePath, "Data");
+var queryCsvBody = CsvEntity.GetDataQueryOrNull(Path.Combine(dataBasePath, "HttpStatusCode.csv"));
+if (queryCsvBody == null)
+{
+    Console.WriteLine("Finish: Not enough.");
+    return;
+}
+
+
+// 置換文字列の辞書作成
+var queryRenameWords = WordEntity.GetDataQueryOrNull(Path.Combine(dataBasePath, "ReplaceDictionary.csv"));
+ImmutableDictionary<string, string> renameWords = queryRenameWords != null
+    ? queryRenameWords.ToImmutableDictionary(item => item.Target, item => item.Rename)
+    : ImmutableDictionary<string, string>.Empty;
+
+
+// データの解析
+ImmutableList<ContentEntity> data = queryCsvBody
+    .Select(item => ContentEntity.Parse(item, renameWords))
+    .Where(item => item != null)
+    .Select(item => item!)
+    .ToImmutableList();
+
+
+// 出力先のディレクトリ再作成
+string outputBasePath = Path.Combine(basePath, "DataOutput");
+if (Directory.Exists(outputBasePath))
+{
+    Directory.Delete(outputBasePath, true);
+}
+Directory.CreateDirectory(outputBasePath);
+
+
+// コード生成
+foreach (var (ext, generator) in generators)
+{
+    File.WriteAllText(
+        Path.Combine(outputBasePath, $"HttpStatusCode.{ext}"),
+        generator.Generate(data)
+    );
+}
+
+
+Console.WriteLine("Finish: Success");
