5325b752a2b2edad95695f83d08a28d675a94133
shion <tenta_shion@yahoo.co.jp>
2022-01-28 17:29:11 +0900

    Kotlin 向けの変換コード追加

--- android/src/main/java/com/github/tshion/mktools_android/constants/HttpStatusCode.kt
+++ android/src/main/java/com/github/tshion/mktools_android/constants/HttpStatusCode.kt
@@ -552,4 +552,24 @@ enum class HttpStatusCode(val value: Int) {
      */
     NETWORK_AUTHENTICATION_REQUIRED(511),
     ;
+
+
+    companion object {
+
+        /**
+         * Returns the element matching the given value.
+         */
+        @JvmStatic
+        fun parse(value: Int): HttpStatusCode {
+            return values().single { it.value == value }
+        }
+
+        /**
+         * Returns the element matching the given value or null.
+         */
+        @JvmStatic
+        fun parseOrNull(value: Int): HttpStatusCode? {
+            return values().singleOrNull { it.value == value }
+        }
+    }
 }
--- generators/HttpStatusCodeGenerator/Templates/root.template.kt
+++ generators/HttpStatusCodeGenerator/Templates/root.template.kt
@@ -7,4 +7,24 @@ enum class HttpStatusCode(val value: Int) {
 
 %%ITEMS%%
     ;
+
+
+    companion object {
+
+        /**
+         * Returns the element matching the given value.
+         */
+        @JvmStatic
+        fun parse(value: Int): HttpStatusCode {
+            return values().single { it.value == value }
+        }
+
+        /**
+         * Returns the element matching the given value or null.
+         */
+        @JvmStatic
+        fun parseOrNull(value: Int): HttpStatusCode? {
+            return values().singleOrNull { it.value == value }
+        }
+    }
 }
