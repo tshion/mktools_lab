dfafc75c7ec33867dc83cc79247a7299cf2b39b2
shion <tenta_shion@yahoo.co.jp>
2022-02-01 17:37:49 +0900

    コード生成時に展開するデータのリネーム

--- generators/GenerateCodeLibrary/SourcePropertyEntity.cs
+++ generators/GenerateCodeLibrary/SourcePropertyEntity.cs
@@ -1,14 +1,14 @@
 namespace GenerateCodeLibrary
 {
     /// <summary>
-    /// コード生成時に繰り返し利用されるデータ
+    /// コード生成時に展開するプロパティ関連データ
     /// </summary>
     /// <param name="Documents">ドキュメントコメント</param>
     /// <param name="Name">プロパティ名</param>
     /// <param name="Prefix">プロパティ宣言の前につける追記事項</param>
     /// <param name="Type">型名</param>
     /// <param name="Value">値</param>
-    public sealed record class CodeItemEntity(
+    public sealed record class SourcePropertyEntity(
         IEnumerable<string> Documents,
         string Name,
         IEnumerable<string> Prefix,
