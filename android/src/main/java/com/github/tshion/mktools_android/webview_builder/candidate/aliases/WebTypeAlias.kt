96aa4391ec7bb4dd4113d91b5056187da27e43e5
shion <tenta_shion@yahoo.co.jp>
2022-02-11 12:52:40 +0900

    WebViewClient の仮実装

--- android/src/main/java/com/github/tshion/mktools_android/webview_builder/candidate/aliases/WebTypeAlias.kt
+++ android/src/main/java/com/github/tshion/mktools_android/webview_builder/candidate/aliases/WebTypeAlias.kt
@@ -9,8 +9,4 @@ import androidx.webkit.SafeBrowsingResponseCompat
 internal typealias MktOnCreateWindowPredicate = (view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?) -> Boolean
 internal typealias MktOnJsPredicate = (view: WebView?, url: String?, message: String?, result: JsResult?) -> Boolean
 internal typealias MktOnJsPromptPredicate = (view: WebView?, url: String?, message: String?, defaultValue: String?, result: JsPromptResult?) -> Boolean
-internal typealias MktOnReceivedErrorConsumer = (view: WebView?, errorCode: Int, description: String?, failingUrl: String?) -> Unit
-internal typealias MktOnReceivedHttpAuthRequestConsumer = (view: WebView?, handler: HttpAuthHandler?, host: String?, realm: String?) -> Unit
-internal typealias MktOnReceivedLoginRequestConsumer = (view: WebView?, realm: String?, account: String?, args: String?) -> Unit
-internal typealias MktOnSafeBrowsingHitConsumer = (view: WebView?, request: WebResourceRequest?, threatType: Int, callback: SafeBrowsingResponseCompat) -> Unit
 internal typealias MktOnShowFileChooserPredicate = (webView: WebView?, filePathCallback: ValueCallback<Array<Uri?>?>?, fileChooserParams: FileChooserParams?) -> Boolean
---
+++ android/src/main/java/com/github/tshion/mktools_android/webview_builder/candidate/aliases/WebViewClientCompatTypeAlias.kt
@@ -0,0 +1,11 @@
+package com.github.tshion.mktools_android.webview_builder.candidate.aliases
+
+import android.webkit.HttpAuthHandler
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import androidx.webkit.SafeBrowsingResponseCompat
+
+
+internal typealias MktOnReceivedHttpAuthRequestConsumer = (view: WebView, handler: HttpAuthHandler, host: String, realm: String) -> Unit
+internal typealias MktOnReceivedLoginRequestConsumer = (view: WebView, realm: String, account: String?, args: String) -> Unit
+internal typealias MktOnSafeBrowsingHitConsumer = (view: WebView, request: WebResourceRequest, threatType: Int, callback: SafeBrowsingResponseCompat) -> Unit
