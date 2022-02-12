1ac24a720df625f03c15342a02914a04fbef7529
shion <tenta_shion@yahoo.co.jp>
2022-02-12 19:13:34 +0900

    View 拡張メソッドの追加

---
+++ android/src/main/java/com/github/tshion/mktools_android/ViewExtensions.kt
@@ -0,0 +1,20 @@
+package com.github.tshion.mktools_android
+
+import android.annotation.SuppressLint
+import android.view.View
+import androidx.core.view.GestureDetectorCompat
+
+/**
+ * Sets the listener to ignore double tap event.
+ *
+ * @receiver View instance.
+ */
+@SuppressLint("ClickableViewAccessibility")
+fun View?.setOnSingleTapTouchListener() {
+    this ?: return
+
+    val detector = GestureDetectorCompat(this.context, IgnoreDoubleTapOnGestureListener())
+    this.setOnTouchListener { _, event ->
+        detector.onTouchEvent(event)
+    }
+}
