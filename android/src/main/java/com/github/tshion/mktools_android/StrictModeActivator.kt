19633b3776f16b4a024c330f04e3a7d9290dc296
shion <tenta_shion@yahoo.co.jp>
2022-02-18 13:48:05 +0900

    VmPolicy ペナルティ緩和のコミット漏れ追記

--- android/src/main/java/com/github/tshion/mktools_android/StrictModeActivator.kt
+++ android/src/main/java/com/github/tshion/mktools_android/StrictModeActivator.kt
@@ -58,7 +58,7 @@ interface StrictModeActivator {
     val vmPolicy: VmPolicy
         get() = VmPolicy.Builder()
             .detectAll()
-            .penaltyDeath()
+            .penaltyLog()
             .build()
 
 
