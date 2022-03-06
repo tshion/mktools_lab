ffc074f7e8f11585ba44da66769e50fd6f428881
shion <tenta_shion@yahoo.co.jp>
2022-03-06 12:26:27 +0900

    可視性の調整
    イベント発火しないので調査のため

--- ios/Sources/UIKit/WebViewBuilder/Proxies/WKNavigationProxyNext.swift
+++ ios/Sources/UIKit/WebViewBuilder/Proxies/WKNavigationProxyNext.swift
@@ -1,14 +1,14 @@
 #if os(iOS)
     import WebKit
 
-    internal class WKNavigationProxyNext: NSObject, WKNavigationDelegate {
+    public class WKNavigationProxyNext: NSObject, WKNavigationDelegate {
 
         internal var didFailProvisionalNavigation: ((WKWebView, WKNavigation, Error) -> Void)? =
             nil
 
 
         // MARK: Responding to Navigation Errors
-        func webView(
+        public func webView(
             _ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!,
             withError: Error
         ) {
