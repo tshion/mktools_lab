42383b8a7a4edd908b26c38d9d1b712dd5de9001
shion <tenta_shion@yahoo.co.jp>
2022-05-05 21:24:39 +0900

    OkHttp Call のsuspend ラッピング

---
+++ android/src/main/java/com/github/tshion/mktools_android/okhttp/OkHttpExtensions.kt
@@ -0,0 +1,28 @@
+package com.github.tshion.mktools_android.okhttp
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/**
+ * Wraps [Call] in suspend.
+ */
+suspend fun Call.toSuspend() = suspendCancellableCoroutine<Response> { continuation ->
+    continuation.invokeOnCancellation {
+        cancel()
+    }
+
+    enqueue(object : Callback {
+        override fun onResponse(call: Call, response: Response) {
+            continuation.resume(response)
+        }
+
+        override fun onFailure(call: Call, e: IOException) {
+            continuation.resumeWithException(e)
+        }
+    })
+}
