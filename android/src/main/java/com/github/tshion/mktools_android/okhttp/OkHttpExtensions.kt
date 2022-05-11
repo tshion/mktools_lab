be19f7567f70ba97fb90e67441611af204b4abad
shion <tenta_shion@yahoo.co.jp>
2022-05-11 20:15:03 +0900

    OkHttp のHeader 関連の拡張メソッド追加

--- android/src/main/java/com/github/tshion/mktools_android/okhttp/OkHttpExtensions.kt
+++ android/src/main/java/com/github/tshion/mktools_android/okhttp/OkHttpExtensions.kt
@@ -1,8 +1,11 @@
 package com.github.tshion.mktools_android.okhttp
 
+import com.github.tshion.mktools_android.constants.HttpRequestHeader
+import com.github.tshion.mktools_android.constants.HttpResponseHeader
 import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 import kotlin.coroutines.resume
@@ -26,3 +29,26 @@ suspend fun Call.await() = suspendCancellableCoroutine<Response> { continuation 
         }
     })
 }
+
+fun Request.Builder.addHeader(
+    type: HttpRequestHeader,
+    value: String,
+) = addHeader(type.name, value)
+
+fun Request.Builder.header(
+    type: HttpRequestHeader,
+    value: String,
+) = header(type.name, value)
+
+fun Request.Builder.removeHeader(
+    type: HttpRequestHeader,
+) = removeHeader(type.name)
+
+fun Response.header(
+    type: HttpResponseHeader,
+    defaultValue: String? = null,
+) = header(type.name, defaultValue)
+
+fun Response.headers(
+    type: HttpResponseHeader,
+) = headers(type.name)
