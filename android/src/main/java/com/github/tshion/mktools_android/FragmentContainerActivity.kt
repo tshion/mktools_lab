0cbd95b172366d39acaa29897a220dc12db6b063
shion <tenta_shion@yahoo.co.jp>
2022-05-11 11:46:33 +0900

    実装ブラッシュアップ

--- android/src/main/java/com/github/tshion/mktools_android/FragmentContainerActivity.kt
+++ android/src/main/java/com/github/tshion/mktools_android/FragmentContainerActivity.kt
@@ -3,38 +3,41 @@ package com.github.tshion.mktools_android
 import android.os.Bundle
 import android.view.ViewGroup.LayoutParams
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
 
 /**
- * Hosts [FragmentContainerView].
+ * Hosts [Fragment].
  */
 abstract class FragmentContainerActivity : AppCompatActivity() {
+
+    protected lateinit var fragmentContainerView: FragmentContainerView
 
     /**
      * Id of [FragmentContainerView].
      */
-    @IdRes
-    protected val idContainer: Int = resources.getIdentifier(
-        "container",
-        "id",
-        packageName
-    )
+    protected val idContainer: Int by lazy {
+        resources.getIdentifier(
+            "mktools_fragment_container_activity_root",
+            "id",
+            packageName
+        )
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        FragmentContainerView(this@FragmentContainerActivity).apply {
+        fragmentContainerView = FragmentContainerView(this@FragmentContainerActivity).apply {
             id = idContainer
             layoutParams = LayoutParams(
                 MATCH_PARENT,
                 MATCH_PARENT,
             )
-        }.also { setContentView(it) }
+        }
+        setContentView(fragmentContainerView)
         setupFragment(idContainer, supportFragmentManager)
     }
 
