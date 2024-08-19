9409f5ff6c694dc38145012d8ddb0006aeffe0c0
tshion <tenta_shion@yahoo.co.jp>
2024-08-19 21:46:46 +0900

    Template 化

---
+++ typespec-converter/input-schema.template.ts
@@ -0,0 +1,39 @@
+// 自動生成
+
+/** 入力形式 */
+export const InputType = {
+    TEXT: 'text',
+} as const;
+
+/** 入力形式の型エイリアス */
+export type InputType = typeof InputType[keyof typeof InputType];
+
+/**
+ * 入力欄の構造定義
+ */
+export interface InputSchemaDto {
+
+    /** 入力形式 */
+    inputType: InputType;
+
+    /** 複数入力できるかどうか */
+    isArray: boolean;
+
+    /** キー名 */
+    key: string;
+
+    /** 概要 */
+    label: string;
+
+    /** 選択肢 */
+    options?: (number | string)[];
+
+    /** (デフォルトの)入力値 */
+    value: (number | string)[];
+}
+
+
+/** 入力欄の構造データ */
+export const inputSchemas: InputSchemaDto[] = [
+    /** Data */
+];
