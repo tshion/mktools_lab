bf74c4df6f025bd41e3758dea5584d6b482e6cab
tshion <tenta_shion@yahoo.co.jp>
2024-08-24 09:11:40 +0900

    FormArray のロジック整理

--- typespec-editor/src/app/views/form.component.ts
+++ typespec-editor/src/app/views/form.component.ts
@@ -12,34 +12,62 @@ import { InputSchemaDto } from '../../input-schema';
     ],
     template: `
     <div>
-        <form (ngSubmit)="onSubmit()" [formGroup]="form" class="pure-form pure-form-aligned">
+        <form (ngSubmit)="onSubmit()" [formGroup]="form" class="pure-form pure-form-stacked">
             @for (schema of schemas; track schema) {
-                <div class="pure-control-group" [formGroup]="form">
-                    <p>{{ schema.key }}</p>
-                    @for (cntl of getControls(schema.key).controls; track cntl) {
+                <div [formArrayName]="schema.key">
+                    <h2>
+                        {{ schema.key }}
+                        @if (hasChange(schema)) {
+                            <span style="color: red;">※変更中</span>
+                        }
+                    </h2>
+                    @if (schema.isArray) {
+                        <button type="button" class="pure-button" (click)="addControl(schema)">追加</button>
+                    }
+                    <ng-container *ngFor="let _ of getControls(schema).controls; let i = index">
                         @switch (schema.inputType) {
                             @case ('checkbox') {
-                                <label [attr.for]="schema.key" class="pure-checkbox">
-                                    <input type="checkbox" [formControlName]="schema.key" [id]="schema.key" />
+                                <label for="{{ schema.key }}-{{ i }}" class="pure-checkbox">
+                                    <input type="checkbox" id="{{ schema.key }}-{{ i }}" [formControlName]="i" />
                                     <span class="pure-form-message-inline">{{ schema.label }}</span>
                                 </label>
                             }
+                            @case ('color') {
+                                <label for="{{ schema.key }}-{{ i }}">{{ schema.label }}</label>
+                                <input type="color" id="{{ schema.key }}-{{ i }}" [formControlName]="i" />
+                                <span class="pure-form-message-inline">{{ getControls(schema).controls[i].getRawValue() }}</span>
+                            }
                             @case ('number') {
-                                <label [attr.for]="schema.key">{{ schema.label }}</label>
-                                <input type="number" step="1" [formControlName]="schema.key" [id]="schema.key" />
+                                <label for="{{ schema.key }}-{{ i }}">{{ schema.label }}</label>
+                                <input type="number" step="1" id="{{ schema.key }}-{{ i }}" [formControlName]="i" />
+                            }
+                            @case ('select') {
+                                <label for="{{ schema.key }}-{{ i }}">{{ schema.label }}</label>
+                                <select id="{{ schema.key }}-{{ i }}" [formControlName]="i">
+                                    @for (opt of schema.options; track opt) {
+                                        <option [value]="opt">{{ opt }}</option>
+                                    }
+                                </select>
+                            }
+                            @case ('textbox') {
+                                <label for="{{ schema.key }}-{{ i }}">{{ schema.label }}</label>
+                                <input type="text" id="{{ schema.key }}-{{ i }}" [formControlName]="i" />
                             }
                         }
-                    }
-                    <button class="pure-button" type="button" (click)="reset(schema)">Reset</button>
+                        @if (schema.isArray) {
+                            <button type="button" class="pure-button" (click)="removeControl(schema, i)">削除</button>
+                        }
+                    </ng-container>
                 </div>
+                <button class="pure-button" type="button" (click)="resetControls(schema)">リセット</button>
             }
-            <div class="form-row">
+            <div>
                 <button class="pure-button pure-button-primary" type="submit" [disabled]="!form.valid">Save</button>
             </div>
         </form>
 
         @if (payLoad) {
-            <div class="form-row">
+            <div>
                 <strong>Saved the following values</strong><br>{{ payLoad }}
             </div>
         }
@@ -71,20 +99,26 @@ export class FormComponent implements OnInit {
     }
 
 
-    public add(data: InputSchemaDto) {
-        (this.form.controls[data.key] as FormArray).push(new FormControl());
+    public addControl(data: InputSchemaDto, value?: any) {
+        this.getControls(data).push(new FormControl(value));
     }
 
-    public getControls(key: string) {
-        return this.form.controls[key] as FormArray;
+    public getControls(data: InputSchemaDto) {
+        return this.form.controls[data.key] as FormArray;
     }
 
-    public remove(key: string, index: number) {
-        (this.form.controls[key] as FormArray).removeAt(index);
+    public hasChange(data: InputSchemaDto) {
+        const input = this.getControls(data).getRawValue();
+        return input.length !== data.value.length
+            || !input.every(x => data.value.includes(x));
     }
 
-    public reset(item: InputSchemaDto) {
-        const controls = item.value.map(x => new FormControl(x));
-        this.form.controls[item.key] = new FormArray(controls);
+    public removeControl(data: InputSchemaDto, index: number) {
+        this.getControls(data).removeAt(index);
+    }
+
+    public resetControls(data: InputSchemaDto) {
+        this.getControls(data).clear();
+        data.value.forEach(x => this.addControl(data, x));
     }
 }
